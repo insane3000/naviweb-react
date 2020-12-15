474285c960b0bbe4a18a8b79ca828738afdd0406
Roger Shoke <samuel18x@gmail.com>
2020-12-14 22:25:10 -0400

    Agregado y primeros pasos

---
+++ src/left.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Left = () => (
+    <div className="left">
+        <h2>Left</h2>
+    </div>
+)
+export default Left 
---
+++ src/right.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Right = () => (
+    <div className="right" >
+        <h2>Right</h2>
+    </div>
+)
+export default Right 
