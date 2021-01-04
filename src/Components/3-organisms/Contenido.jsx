3c0b9aa57ed90195d2afb2887751e4a66a6a63c9
Roger Shoke <samuel18x@gmail.com>
2021-01-04 18:36:56 -0400

    agregado el titulo de promociones

---
+++ src/Components/3-organisms/Contenido.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from "react"
+
+class Contenido extends Component {
+
+    // constructor(props) {
+    //     super(props)
+    // }
+
+    render() {
+        return (
+            <div className="juegos-actualizados">
+                <h2 className={this.props.className}>{this.props.titulo}</h2>
+                <span>{this.props.contenido1} <strong>{this.props.negrita}</strong> <br/>
+                {this.props.contenido2}
+                </span>
+            </div>
+        )
+    }
+
+
+
+
+}
+
+
+export default Contenido
--- src/Components/4-templates/Right.jsx
+++ src/Components/4-templates/Right.jsx
@@ -1,13 +1,28 @@
 import React from 'react'
 import Slider from '../3-organisms/Slider'
 import Games from '../3-organisms/Games'
-import JuegosActualizados from  '../3-organisms/JuegosActualizados'
+import Contenido from '../3-organisms/Contenido'
 
 const Right = () => (
     <div className="right" >
         <Slider />
-        <JuegosActualizados />
+        <Contenido 
+        className="juegos-color-h2"
+        titulo="JUEGOS ACTUALIZADOS" 
+        contenido1="A continuación te mostramos los juegos más populares de" 
+        negrita="Navi Games Cbba"
+        contenido2="Si alguno no está actualizado, con solo notificar al que 
+        atiende tu juego estará actualizado en cuestión de minutos."
+        />
         <Games />
+        <Contenido 
+        className="promos-color-h2"
+        titulo="NUESTRAS PROMOCIONES" 
+        // contenido1="No tienes que perderte!" 
+        negrita="No tienes que perderte!"
+        contenido2="Nuestras promociones son salvajes , a continuación te mostramos algunas..."
+        />
+
     </div>
 )
 export default Right 
