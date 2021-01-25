bc7841704f699c056c16802c3a4c4c1abd4e9bae
Roger Shoke <samuel18x@gmail.com>
2021-01-25 16:47:47 -0400

    cambios

--- src/Components/3-organisms/Intro.jsx
+++ src/Components/3-organisms/Intro.jsx
@@ -1,23 +1,19 @@
-import React, { Component } from "react"
+import React from "react"
 import mascota from '../../img/mascota.webp';
-class Intro extends Component {
 
-    // constructor(props) {
-    //     super(props)
-    // }
+const Intro = () => {
 
-    render() {
-        return (
-            <div className="intro" id="intro">
-                <div className="intro-data">
-                    <h2>Navi Games Cbba</h2>
-                    <h3>Un lugar tranquilo para jugar!</h3>
-                </div>
-                <div className="intro-img">
-                    <img src={mascota} alt="mascota" />
-                </div>
+    return (
+        <div className="intro" id="intro">
+            <div className="intro-data">
+                <h2>Navi Games Cbba</h2>
+                <h3>Un lugar tranquilo para jugar!</h3>
             </div>
-        )
-    }
+            <div className="intro-img">
+                <img src={mascota} alt="mascota" />
+            </div>
+        </div>
+    )
+
 }
 export default Intro
