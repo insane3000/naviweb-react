78ff2c27f15902ae39a2dba6ef1bcaa80eeccaa3
Roger Shoke <samuel18x@gmail.com>
2021-02-10 17:32:31 -0400

    changes

--- src/Components/3-organisms/Intro.jsx
+++ src/Components/3-organisms/Intro.jsx
@@ -7,7 +7,7 @@ const Intro = () => {
         <div className="intro" id="intro">
             <div className="intro-data">
                 <h2>Navi Games Cbba</h2>
-                <h3>Un lugar tranquilo para jugar!</h3>
+                <h3>Juega desde <strong>2Bs</strong> la hora... <br/> Con tu cuenta <strong>Inmortal.</strong></h3>
             </div>
             <div className="intro-img">
                 <img src={mascota} alt="mascota" />
